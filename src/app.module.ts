95c172c6403260b5a93fedb38df4c1fecd6b18b8
nishyjana <nishyjana6435@gmail.com>
2022-08-27 15:45:41 +0530

    initial setup commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from './common/helper/env.helper';
+
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
